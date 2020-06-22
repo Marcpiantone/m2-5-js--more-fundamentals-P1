22544effe76f3dd22f70fd066b6d87bdb1487752
“Marc <marc_piantone@live.fr>
2020-06-22 17:16:47 -0400

    Answer exercise-3-1

--- workshop/exercises-P3_problems/exercise-3-1/app.js
+++ workshop/exercises-P3_problems/exercise-3-1/app.js
@@ -6,3 +6,37 @@
 // Guidelines
 // - write only one event listener
 // - use a switch statement
+
+//Grab the html tag we'll be using to add our style
+
+const button = document.querySelector("ul");
+
+// for button 4 we should add a .jitters class
+
+let myAnimations = function (event) {
+  const buttonId = event.target.id;
+  const selector = function (buttonId) {
+    return document.getElementById(buttonId);
+  };
+
+  console.log(buttonId);
+
+  switch (buttonId) {
+    case "btn-1":
+      selector(buttonId).style.opacity = "0";
+      break;
+    case "btn-2":
+      selector(buttonId).style.background = "crimson";
+      break;
+    case "btn-3":
+      selector(buttonId).style.background = "lightblue";
+      break;
+    case "btn-4":
+      selector(buttonId).classList.add("jitters");
+      break;
+  }
+};
+
+// One event listener to rule them all
+
+button.addEventListener("click", myAnimations);
