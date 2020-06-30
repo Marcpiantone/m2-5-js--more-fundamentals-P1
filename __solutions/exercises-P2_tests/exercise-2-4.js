6b8c450f8c8492769e7cb681050af34616e8c7b7
“Marc <marc_piantone@live.fr>
2020-06-29 21:21:30 -0400

    Answer exercise-2-4

--- __solutions/exercises-P2_tests/exercise-2-4.js
+++ __solutions/exercises-P2_tests/exercise-2-4.js
@@ -9,6 +9,6 @@
 
 function f(arr) {
   const letter = arr[0].charAt(arr[1]);
-  if (letter === '') return undefined;
+  if (letter === "") return undefined;
   return letter;
 }
--- workshop/exercises-P2_tests/exercise-2-4.js
+++ workshop/exercises-P2_tests/exercise-2-4.js
@@ -10,12 +10,13 @@
 // getLetterAtIndex("abc", 0);   // a
 
 function getLetterAtIndex(str, index) {
-  // Your code here
+  return str[index];
 }
 
 // Add 6 more test cases
-expect(getLetterAtIndex('hello', 4), 'o');
-expect(getLetterAtIndex('goodbye', 0), 'g');
+expect(getLetterAtIndex("hello", 4), "o");
+expect(getLetterAtIndex("goodbye", 0), "g");
+expect(getLetterAtIndex("", 4), undefined);
 
 /**
  * -------------------------------------------------------------------
@@ -24,7 +25,7 @@ expect(getLetterAtIndex('goodbye', 0), 'g');
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
